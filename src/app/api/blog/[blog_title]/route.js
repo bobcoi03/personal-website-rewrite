ae142bcb4572d423b3b4d60118f7ed942256661f
bobcoi03 <elongatedmusk6969@gmail.com>
2023-07-10 15:43:39 +0100

    export vercel edge functions

--- src/app/api/blog/[blog_title]/route.js
+++ src/app/api/blog/[blog_title]/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server"
 
+export const config = {
+    runtime: 'edge',
+  };
+
 export const blogsData = {
     "joyy": {
         title: "Joyy Inc: Undervalued social media",
