bfae2bc5ad4cc644a04eb1abb4295e18fb459594
bobcoi03 <elongatedmusk6969@gmail.com>
2023-07-10 17:55:47 +0100

    .

--- src/app/api/blog/[blog_title]/route.js
+++ src/app/api/blog/[blog_title]/route.js
@@ -1,8 +1,6 @@
-import { NextResponse } from "next/server"
+export const dynamic = 'force-dynamic'
 
-export const config = {
-    runtime: 'edge',
-  };
+import { NextResponse } from "next/server"
 
 export const blogsData = {
     "joyy": {
