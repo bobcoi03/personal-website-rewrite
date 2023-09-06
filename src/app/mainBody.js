01ffc9b5ce389468c7154ddf3554cada860f2bf3
bobcoi03 <elongatedmusk6969@gmail.com>
2023-09-06 14:04:23 +0100

    got rid of bloat

--- src/app/mainBody.js
+++ src/app/mainBody.js
@@ -6,24 +6,10 @@ import { useEffect, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MainBody({ children }) {
-    const [initialTheme, setInitialTheme] = useState("")
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const hasVisited = localStorage.getItem("visited")
-
-            if (!hasVisited) {
-                setInitialTheme("light")
-            } else {
-                setInitialTheme(localStorage.getItem("data-theme"))
-            }
-        }
-    }, [])
-
     return (
         <body 
             className={inter.className} 
-            data-theme={initialTheme}
+            data-theme={"light"}
             id="main-body"
         >
             {children}
--- src/components/nav.js
+++ src/components/nav.js
@@ -17,18 +17,6 @@ export default function Nav() {
                 <Link href="/blogs" className="btn btn-ghost normal-case">
                     Blog
                 </Link>
-                <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                        <a onClick={()=>window.my_modal_2.showModal()}>
-                        Themes
-                        </a>
-                    </li>
-                </ul>
-                </div>
             </div>
             <ThemePickerModal/>
         </div>
